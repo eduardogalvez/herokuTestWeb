9b3a6e0d00010429afa4ab4e49c9fe2c1b75d858
Ha Ha <eduardogalvez@umail.ucsb.edu>
2020-01-26 17:18:10 -0800

    updated ui

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -1,11 +1,11 @@
 app.controller('MainController', ['$scope', '$http', function($scope, $http) { 
   	$scope.todo = {
-  		title: "Things I Need to Do",
-  		list: ["Clean my room", "Go to the store", "Study Cracking the Coding Interview"]
+  		title: "SpotifyMatcher",
+  		//list: ["Clean my room", "Go to the store", "Study Cracking the Coding Interview"]
   	}
 
   	$scope.books = {
-  		title: "Books I Need to Buy",
+  		title: "Add Song",
   		list: []
   	}
 
