440956ad1a5de085a72407421e7509950d7155d1
Ha Ha <eduardogalvez@umail.ucsb.edu>
2020-01-26 18:16:07 -0800

    hopefully able to get playlist

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -12,18 +12,49 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http) {
     function hasOnlyNumbers(item) {
       return /^[0-9]*$/.test(item)
     }
+
+
+
+    
   	
-    $scope.addItem = function(itemList, item) {
+   // $scope.addItem = function(itemList, item) {
       // ISBN : 10 or 13 length and consisdt of only numbers
-      if ((item.length == 10 || item.length == 13) && hasOnlyNumbers(item)) {
-        console.log("ISBN");
-        $http.get("https://www.googleapis.com/books/v1/volumes?q=isbn:" + item).success(function(data) {
-          itemList.push("Title: " + data.items[0].volumeInfo.title + " // Author(s):" + data.items[0].volumeInfo.authors)
-        })
-      } else {
-        console.log("Not an ISBN")
-        itemList.push(item);
-      }
-  	}
+    //  if ((item.length == 10 || item.length == 13) && hasOnlyNumbers(item)) {
+      //  console.log("ISBN");
+        //$http.get("https://www.googleapis.com/books/v1/volumes?q=isbn:" + item).success(function(data) {
+         // itemList.push("Title: " + data.items[0].volumeInfo.title + " // Author(s):" + data.items[0].volumeInfo.authors)
+       // })
+      //} else {
+       // console.log("Not an ISBN")
+       // itemList.push(item);
+     // }
+   // }
+
+
+   var SpotifyWebApi = require('spotify-web-api-node');
+
+  // credentials are optional
+  var spotifyApi = new SpotifyWebApi({
+   // clientId: 'fcecfc72172e4cd267473117a17cbd4d',
+   // clientSecret: 'a6338157c9bb5ac9c71924cb2940e1a7',
+    redirectUri: 'http://www.example.com/callback'
+  });
+
+
+  
+    $scope.addItem = function(itemList, item) {
+      spotifyApi.getUserPlaylists(item)
+  .then(function(data) {
+    console.log('Retrieved playlists', data.body);
+    itemList.push(data.body);
+
+  },function(err) {
+    console.log('Something went wrong!', err);
+  })
+
+    }
+    
+    
+
 
 }]);
