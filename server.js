0b896481d46838ee5792d847040ee3e0e4a9302b
Ha Ha <eduardogalvez@umail.ucsb.edu>
2020-02-01 20:28:39 -0800

    possibly able to run nodejs script

--- server.js
+++ server.js
@@ -1,23 +1,8 @@
-var express = require('express')
+var express = require('express');
 var app = express();
-var http = require('http')
+var port = 4000;
 
-http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-  }).listen(8888);
+app.listen(process.env.port || port);
 
-
-var port = process.env.PORT || 8080;
-
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function(req, res) {
-	res.render('index');
-})
-
-app.listen(port, function() {
-	console.log('app running')
-})
+node port=3000 /web-api-auth-examples/authorization_code/app.js
 
