54807d5e8a69f055d812dff46d7f8d5313e8bee7
Ha Ha <eduardogalvez@umail.ucsb.edu>
2020-02-01 20:38:41 -0800

    trying to run script

--- server.js
+++ server.js
@@ -2,7 +2,20 @@ var express = require('express');
 var app = express();
 var port = 4000;
 
-app.listen(process.env.port || port);
 
 node port=3000 /web-api-auth-examples/authorization_code/app.js
+app.listen(process.env.port || port);
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.render('index');
+})
+
+app.listen(port, function() {
+	console.log('app running')
+})
+
+
 
